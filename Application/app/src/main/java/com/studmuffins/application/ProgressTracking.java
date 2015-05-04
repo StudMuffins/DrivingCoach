e62a779747663b5366aa41c639e28921475f3707
Zoe Sanderson-Wall <zoewall1104@gmail.com>
2015-05-04 09:02:34 +0200

    adding score for merge

--- Application/app/src/main/java/com/studmuffins/application/ProgressTracking.java
+++ Application/app/src/main/java/com/studmuffins/application/ProgressTracking.java
@@ -1,70 +1,122 @@
 package com.studmuffins.application;
+import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by Jono and Zoe on 1/05/2015.
  */
-public class ProgressTracking extends Fragment {
-    private HashMap <String, Integer> fuelMap = new HashMap<String, Integer>();
-    private HashMap <String, Integer> gearMap = new HashMap<String, Integer>();
-    private HashMap <String, Integer> brakeMap = new HashMap<String, Integer>();
+public class ProgressTracking extends Activity {
+    private HashMap <String, String> scoreMap = new HashMap<String, String>();
 
-    private  String dateKey;
-    private String score_gear = "11";
+    private String gear = "gear";
+    private String fuel = "fuel";
+    private String brake = "brake";
+    private String fuelScore, gearScore, brakeScore;
     private TextView text_gear;
-    private String score_fuel = "50";
     private TextView text_fuel;
+    private TextView text_brake;
+    private LinearLayout fuelLayout, gearLayout, brakeLayout;
 
-    //constructor
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.progress_tracking, container, false);
+    // constructor
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.progress_tracking);
 
-        //setScoreStrings();
-        text_gear = (TextView) view.findViewById(R.id.gearScore_text);
-        text_gear.setText(score_gear);
-        text_fuel = (TextView) view.findViewById(R.id.fuelScore_text);
-        text_fuel.setText(score_fuel);
+        // Set String values
+        setFuelScore();
+        setBrakeScore();
+        setGearScore();
 
-        return view;
+        // Set background colours
+        setBrakeColour();
+        setFuelColour();
+        setGearColour();
+
+        // Set text in text fields
+        text_gear = (TextView) findViewById(R.id.gearScore_text);
+        text_gear.setText(getGearScore());
+        text_fuel = (TextView) findViewById(R.id.fuelScore_text);
+        text_fuel.setText(getFuelScore());
+        text_brake = (TextView) findViewById(R.id.brakeScore_text);
+        text_brake.setText(getBrakeScore());
     }
 
     // Get and Set the fuel score
-    public int getFuelValue() {
-        //fuelMap.put(String, //Value);
-        return fuelMap.get(dateKey);
+    public String getFuelScore() {
+        return scoreMap.get(fuel);
     }
-    public void setFuelValue(int fuelScore) {
-        fuelMap.put(dateKey, fuelScore);
+    public void setFuelScore() {
+        fuelScore = Integer.toString((30 + (int)(Math.random()*71)));
+        scoreMap.put(fuel, fuelScore);
     }
 
     // Get and Set the gear changing score
-    public int getGearValue(){
-        //gearMap.put(String, //Value);
-        return gearMap.get(dateKey);
+    public String getGearScore(){
+        return scoreMap.get(gear);
     }
-    public void setGearValue(int gearScore) {
-        gearMap.put(dateKey, gearScore);
+    public void setGearScore() {
+        gearScore = Integer.toString((30 + (int)(Math.random()*71)));
+        scoreMap.put(gear, gearScore);
     }
 
     // Get and Set the braking score
-    public int getBrakeValue(){
-        //brakeMap.put(String, //Value);
-        return brakeMap.get(dateKey);
+    public String getBrakeScore(){
+        return scoreMap.get(brake);
     }
-    public void setBrakeValue(int brakeScore) {
-        brakeMap.put(dateKey, brakeScore);
+    public void setBrakeScore() {
+        brakeScore = Integer.toString((30 + (int)(Math.random()*71)));
+        scoreMap.put(brake, brakeScore);
     }
 
-    public void setScoreStrings() {
-        score_gear = Integer.toString(getGearValue());
-        score_fuel = Integer.toString(getFuelValue());
+    public void setGearColour() {
+        // Connect to layouts through id reference
+        gearLayout = (LinearLayout) findViewById(R.id.gear_layout);
+        int i = Integer.parseInt(getGearScore());
+        if(i < 50)
+            gearLayout.setBackgroundColor(Color.parseColor("#FF0A0A"));
+        else if(i >= 50 && i < 65)
+            gearLayout.setBackgroundColor(Color.parseColor("#FF6C0A"));
+        else if(i >= 65 && i < 80)
+            gearLayout.setBackgroundColor(Color.parseColor("#F7F445"));
+        else
+            gearLayout.setBackgroundColor(Color.parseColor("#23FF0A"));
     }
+
+    public void setBrakeColour() {
+        brakeLayout = (LinearLayout) findViewById(R.id.brake_layout);
+        int i = Integer.parseInt(getBrakeScore());
+        if(i < 50)
+            brakeLayout.setBackgroundColor(Color.parseColor("#FF0A0A"));
+        else if(i >= 50 && i < 65)
+            brakeLayout.setBackgroundColor(Color.parseColor("#FF6C0A"));
+        else if(i >= 65 && i < 80)
+            brakeLayout.setBackgroundColor(Color.parseColor("#F7F445"));
+        else
+            brakeLayout.setBackgroundColor(Color.parseColor("#23FF0A"));
+    }
+
+    public void setFuelColour() {
+        fuelLayout = (LinearLayout) findViewById(R.id.fuel_layout);
+        int i = Integer.parseInt(getFuelScore());
+        if(i < 50)
+            fuelLayout.setBackgroundColor(Color.parseColor("#FF0A0A"));
+        else if(i >= 50 && i < 65)
+            fuelLayout.setBackgroundColor(Color.parseColor("#FF6C0A"));
+        else if(i >= 65 && i < 80)
+            fuelLayout.setBackgroundColor(Color.parseColor("#F7F445"));
+        else
+            fuelLayout.setBackgroundColor(Color.parseColor("#23FF0A"));
+    }
+
 }
