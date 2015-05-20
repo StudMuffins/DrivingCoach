627fc47697b33d4a9d43bbcccc4d5c1986415c9a
Zoe Sanderson-Wall <zoewall1104@gmail.com>
2015-05-20 12:58:03 +0200

    ui with adding a new car stuff

--- Application/app/src/main/java/com/studmuffins/application/AddNewCar.java
+++ Application/app/src/main/java/com/studmuffins/application/AddNewCar.java
@@ -5,14 +5,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 /**
  * Created by Zoe on 19/05/2015.
  */
 public class AddNewCar extends BaseActivity {
     private String[] navMenuTitles;
-    TTSManager ttsManager = null;
     EditText carNameText;
+    RadioButton manual, auto;
+    RadioButton petrol, diesel, gas, el;
+    RadioButton[] transmission;
+    RadioButton[] fuel;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,13 @@ public class AddNewCar extends BaseActivity {
         set(navMenuTitles);
 
         carNameText = (EditText)findViewById(R.id.text_carname);
+        manual = (RadioButton)findViewById(R.id.radioButton_manual);
+        auto = (RadioButton)findViewById(R.id.radioButton_auto);
+        petrol = (RadioButton)findViewById(R.id.radioButton_petrol);
+        diesel = (RadioButton)findViewById(R.id.radioButton_diesel);
+        gas = (RadioButton)findViewById(R.id.radioButton_gas);
+        el = (RadioButton)findViewById(R.id.radioButton_el);
+        addRB();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,8 +55,27 @@ public class AddNewCar extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void saveCarName(View v) {
+    public void saveCar(View v) {
         String carName = carNameText.getText().toString();
+
+
         //System.out.println(carName);
+    }
+    protected void getCheckedRB() {
+        for (int i = 0; i < transmission.length; i++) {
+            if(transmission[i].isChecked()) {
+                //String t = transmission[i].getText();
+            }
+        }
+
+    }
+    protected void addRB() {
+        transmission[0] = manual;
+        transmission[1] = auto;
+
+        fuel[0] = petrol;
+        fuel[1] = diesel;
+        fuel[2] = gas;
+        fuel[3] = el;
     }
 }
