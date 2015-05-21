bcf459699f7bf3a4418bb7fe931a2cfd40375412
jhallen711 <jhallen711@gmail.com>
2015-05-21 14:09:23 +0200

    finished the splashscreen

--- Application/app/src/main/java/com/studmuffins/application/SplashScreen.java
+++ Application/app/src/main/java/com/studmuffins/application/SplashScreen.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.content.Intent;
 /**
  * Created by Jonathan on 2015-05-21.
  */
@@ -20,8 +19,10 @@ public class SplashScreen extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
+
 
                 finish();
             }
