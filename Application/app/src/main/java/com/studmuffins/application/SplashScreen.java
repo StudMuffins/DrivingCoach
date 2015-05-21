01d59dd9403f6ecad72ab6328a5879101db95381
jhallen711 <jhallen711@gmail.com>
2015-05-21 13:09:45 +0200

    splashscreen done

---
+++ Application/app/src/main/java/com/studmuffins/application/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.studmuffins.application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+/**
+ * Created by Jonathan on 2015-05-21.
+ */
+public class SplashScreen extends Activity {
+
+    private static int SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SCREEN_DELAY);
+    }
+}
